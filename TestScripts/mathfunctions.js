09ccfcff5f5ca436e4d9ea5e6f0266e18b6ecda8
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-06-02 22:57:23 -0500

    Add files via upload

--- TestScripts/mathfunctions.js
+++ TestScripts/mathfunctions.js
@@ -1,15 +1,25 @@
+//add 2 values together
 function add(num1, num2) {
 	return num1+num2;
 }
 
+//subtract 2 values
 function subtract(num1, num2) {
 	return num1-num2;
 }
 
+//multiply 2 values
 function multiply(num1, num2) {
 	return num1*num2;
 }
 
+//divide 2 values. Note: does not consider divide by 0
+//TODO: assert divide by 0? raise exception?
 function divide(num1, num2) {
 	return num1/num2;
+}
+
+//squares the given value
+function square(num1) {
+	return num1*num1;
 }
