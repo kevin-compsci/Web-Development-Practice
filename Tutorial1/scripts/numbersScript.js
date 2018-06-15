1a85b212c184a955be9ad700e92b8dff1e88eda2
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-06-14 20:12:27 -0500

    Add files via upload

--- Tutorial1/scripts/numbersScript.js
+++ Tutorial1/scripts/numbersScript.js
@@ -14,5 +14,27 @@ var resetButton;
 
 //functions
 function checkGuess() {
-  alert('I am a placeholder');
+  var userGuess = Number(guessField.value);
+  //???
+  if(guessCount == 1) {
+    guesses.textContent = 'Previous guesses: ';
+  }
+  guesses.textContent += userGuess + ' ' //convert to string for output
+
+  //correct guess
+  if(userGuess === randomNumber) {
+    lastResult.textContent = 'Congratulations! You guessed correctly!'
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textcontent = '';
+    setGameOver();
+  } //gameover
+  else if(guessCount === 10) {
+    lastResult.textContent = '!!!GAME OVER!!!';
+    setGameOver();
+  } //try again
+  else {
+    lastResult.textcontent = 'Wrong!';
+    lastResult.style.backgroundColor = 'red';
+  }
+
 }
