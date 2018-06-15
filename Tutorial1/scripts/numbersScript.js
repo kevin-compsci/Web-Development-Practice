182fade98c7874b085ef7ab8970cc6afdcf53ccc
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-06-14 20:30:34 -0500

    Add files via upload

--- Tutorial1/scripts/numbersScript.js
+++ Tutorial1/scripts/numbersScript.js
@@ -62,5 +62,21 @@ function setGameOver() {
 
 //reset game logic
 function resetGame() {
-  
+  guessCount = 1;
+
+  var resetParas = document.querySelectorAll('.resultParas p');
+  for(var i = 0; i < resetParas.length, i++) {
+    resetParas[i].textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+
+  lastResult.style.backgroundColor = 'white';
+
+  randomNumber = Math.floor(Math.random()*100 )+ 1;
 }
