727fc9fa83d8cdb5e13bfa5ec4861c3c4218b6af
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-06-19 18:55:33 -0500

    Update numbersScript.js

--- Tutorial1/scripts/numbersScript.js
+++ Tutorial1/scripts/numbersScript.js
@@ -1,14 +1,10 @@
 //variables and local declarations
-
 var randomNumber = Math.floor(Math.random() * 100) + 1;
-
 var guesses = document.querySelector('.guesses');
 var lastResult = document.querySelector('.lastResult');
 var lowOrHi = document.querySelector('.lowOrHi');
-
 var guessSubmit = document.querySelector('.guessSubmit');
 var guessField = document.querySelector('.guessField');
-
 var guessCount = 1;
 var resetButton;
 
