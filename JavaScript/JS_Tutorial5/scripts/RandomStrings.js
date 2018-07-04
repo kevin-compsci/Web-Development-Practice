1c3093482ffb922ac7c8992348a8714cc5824427
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-07-03 21:54:42 -0500

    Add files via upload

--- JavaScript/JS_Tutorial5/scripts/RandomStrings.js
+++ JavaScript/JS_Tutorial5/scripts/RandomStrings.js
@@ -1,1 +1,9 @@
-//string manipulation practice
+//string manipulation practice in DOM
+
+var str = 'The revolution is dull!';
+str;
+
+//var badStr = test;
+//var badStr2 = 'Test;
+//var badStr3 = Test';
+
