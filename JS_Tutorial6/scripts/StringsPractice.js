0cc3a397d1e19581e36bc1001bffe97b916217ee
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-07-08 19:49:57 -0500

    Add files via upload

---
+++ JS_Tutorial6/scripts/StringsPractice.js
@@ -0,0 +1,23 @@
+//Test in DOM, String practice manipulations by Mozilla JS Tutorials
+
+//STRING LENGTH
+var browser = "chrome";
+alert(browser.length);
+
+//RETRIEVE Nth INDEX IN STRING (OR ARRAY)
+alert(browser[0]) //first index
+alert(browser[browser.length-1]); //last index
+
+//RETRIEVE SUBSTRING INDEX
+alert(browser.indexOf("rom")); //find index for given substring
+alert(browser.indexOf("Cookies")); //-1 because it doesn't exist
+
+if(browser.indexOf("rom") !== -1) {
+	//substring exists
+	alert("Substring does exist!");
+}
+
+alert(browser.slice(0,2)); //returns "ch" where 0 inclusive to n-1th index
+alert(browser.slice(2)); //returns "rome"
+
+//CHANGE CASE
