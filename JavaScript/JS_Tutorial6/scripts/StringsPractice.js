644e5cce082a6fc74060cf542861f918dd6cf297
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-07-08 19:44:37 -0500

    Add files via upload

--- JavaScript/JS_Tutorial6/scripts/StringsPractice.js
+++ JavaScript/JS_Tutorial6/scripts/StringsPractice.js
@@ -1,9 +1,14 @@
 //Test in DOM
 
 //String length
-var browser = 'chrome';
+var browser = "chrome";
 alert(browser.length);
 
 //retrieve nth index in string (or array)
 alert(browser[0]) //first index
 alert(browser[browser.length-1]); //last index
+
+//retrieve substring index
+alert(browser.indexOf("rom")); //find index for given substring
+alert(browser.indexOf("Cookies")); //-1 because it doesn't exist
+
