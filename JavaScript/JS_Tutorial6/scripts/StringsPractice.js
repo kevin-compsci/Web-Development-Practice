73c5cab412d9862552d2f8f3f31780075115beff
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-07-08 19:41:47 -0500

    Add files via upload

--- JavaScript/JS_Tutorial6/scripts/StringsPractice.js
+++ JavaScript/JS_Tutorial6/scripts/StringsPractice.js
@@ -1,1 +1,9 @@
 //Test in DOM
+
+//String length
+var browser = 'chrome';
+alert(browser.length);
+
+//retrieve nth index in string (or array)
+alert(browser[0]) //first index
+alert(browser[browser.length-1]); //last index
