277655becdbeaa5694ea57506f20afb9e9c25de4
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-07-13 20:14:54 -0500

    Add files via upload

--- JavaScript/JS_Tutorial7/scripts/arraysPractice.js
+++ JavaScript/JS_Tutorial7/scripts/arraysPractice.js
@@ -1,1 +1,9 @@
 //Test in DOM, Array practice
+
+var shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+alert(shopping);
+
+//arrays can hold a variety of values
+var random = ["TREE", 748, 'c', [0,1], "Test"];
+
+//ACCESSING AND MODIFYING ARRAY ITEMS
