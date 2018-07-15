6462e8a6fa870fe49c2c2bf1b039c5e3afc53f40
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-07-14 20:50:06 -0500

    Add files via upload

--- JavaScript/JS_Tutorial8/scripts/SillyStoryScript.js
+++ JavaScript/JS_Tutorial8/scripts/SillyStoryScript.js
@@ -8,16 +8,33 @@ function randomValueFromArray(array){
   return array[Math.floor(Math.random()*array.length)];
 }
 
-var storyText = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+var storyText = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
 var insertX = "Willy the Goblin
 Big Daddy
-Father Christmas"
+Father Christmas";
 
 var insertY = "the soup kitchen
 Disneyland
-the White House"
+the White House";
 
 var insertZ = "spontaneously combusted
 melted into a puddle on the sidewalk
-turned into a slug and crawled away"
+turned into a slug and crawled away";
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value != '') {
+    var name = customName.value;
+  }
+
+  if(document.getElementById("uk").checked) {
+    var weight = Math.round(300);
+    var temperature =  Math.round(94);
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
